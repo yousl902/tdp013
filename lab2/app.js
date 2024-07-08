7f5de1f4519524c471b2a709c1b6e6a1569f617f
yousl902 <younes.sleibi2000@gmail.com>
2024-07-08 15:58:24 +0200

    to prevent mongoDB injection attacks

--- lab2/app.js
+++ lab2/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import  mongoSanitize from 'express-mongo-sanitize';
 
 const app = express();
 
@@ -23,6 +24,18 @@ const startServer = async () => {
 
   const postsRouter = await import("./routes/posts.js");
   app.use("/posts", postsRouter.default);
+
+  // this is to sanitize the request body
+  // this will remove all the keys starting with $ and . from the request body
+  // it is important to use this to prevent NoSQL injection
+  // Not so important for this lab, could be tested in the next one
+  app.use(
+    mongoSanitize({
+      onSanitize: ({ req, key }) => {
+        console.warn(`This request[${key}] is sanitized`, req);
+      },
+    }),
+  );
 
   app.listen(3000, () => console.log("Server Started"));
 };
